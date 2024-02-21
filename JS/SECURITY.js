f036d457ec6a9e1bff9f51d08dcdd2738c201691
Exhausted2025 <160739576+Exhausted2025@users.noreply.github.com>
2024-02-21 15:28:12 +0300

    Add files via upload

---
+++ JS/SECURITY.js
@@ -0,0 +1,13 @@
+/*var password;
+
+var pass1="Oberlander99";
+
+password=prompt('ERISIM SIFRESINI GIRINIZ.',' ');
+
+if (password==pass1)
+    alert('SIFRE DOGRULANDI.');
+else
+    {
+        window.location="MAIN_PAGE.html";
+    }
+    *\
